436a1bb6b6ecce919ecccd6b4593828d6d339f3d
EmirPetek <76615295+EmirPetek@users.noreply.github.com>
2024-10-11 18:53:57 +0300

    Make Analysis page design completed

---
+++ app/src/main/java/com/emirpetek/instagramunfollowtracker/ui/fragment/makeAnalysis/MakeAnalysisViewModel.kt
@@ -0,0 +1,7 @@
+package com.emirpetek.instagramunfollowtracker.ui.fragment.makeAnalysis
+
+import androidx.lifecycle.ViewModel
+
+class MakeAnalysisViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
