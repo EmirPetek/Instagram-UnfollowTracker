cf64ee09131674f2a97ff91f3c920729d31d9379
EmirPetek <76615295+EmirPetek@users.noreply.github.com>
2024-10-20 12:18:12 +0300

    Listing user and paging to instagram task completed

--- app/src/main/java/com/emirpetek/instagramunfollowtracker/ui/adapter/FollowersFragmentAdapter.kt
+++ app/src/main/java/com/emirpetek/instagramunfollowtracker/ui/adapter/FollowersFragmentAdapter.kt
@@ -1,10 +1,15 @@
 package com.emirpetek.instagramunfollowtracker.ui.adapter
 
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.emirpetek.instagramunfollowtracker.R
 import com.emirpetek.instagramunfollowtracker.data.DataItem
@@ -18,6 +23,7 @@ class FollowersFragmentAdapter(
     inner class CardViewHolder(view: View): RecyclerView.ViewHolder(view){
         val tvUsername : TextView = view.findViewById(R.id.textViewCardUserName)
         val tvTimestamp : TextView = view.findViewById(R.id.textViewCardUnixtimestamp)
+        val cardUser: CardView = view.findViewById(R.id.cardUserInformation)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolder {
@@ -35,7 +41,22 @@ class FollowersFragmentAdapter(
         for (i in item.string_list_data){
             holder.tvUsername.text = i.value
             holder.tvTimestamp.text = CalculateTime(mContext).unixtsToDate(i.timestamp.toString())
+            holder.cardUser.setOnClickListener { it ->
+                val profileURL = i.href
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse(profileURL)
+
+                intent.setPackage("com.instagram.android")
+                try {
+                    mContext.startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    // Eğer Instagram yüklü değilse tarayıcıda açar
+                    mContext.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(profileURL)))
+                }
+            }
         }
+
+
 
     }
 
